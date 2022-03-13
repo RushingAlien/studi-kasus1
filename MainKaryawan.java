204bd5c5d1074794571c689eb503c78fe1b7aae6
RushingAlien <rushing27alien@gmail.com>
2022-03-13 14:21:09 +0700

    -

---
+++ MainKaryawan.java
@@ -0,0 +1,29 @@
+package studikasus;
+
+import java.util.Scanner;
+
+public class MainKaryawan {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah karyawan   : ");
+        int jumlahKaryawan = scan.nextInt(); scan.nextLine();
+        System.out.println();
+
+        for (int i = 0; i < jumlahKaryawan; i++) {
+            System.out.print("Masukkan nama karyawan     : ");
+            String nama = scan.nextLine();
+            System.out.print("Masukkan alamat            : ");
+            String alamat = scan.nextLine();
+            System.out.print("Masukkan nomor telepon     : ");
+            String nomorTelepon = scan.nextLine();
+            System.out.print("Masukkan jenis kelamin     : ");
+            String jenisKelamin = scan.nextLine();
+            System.out.print("Masukkan kategori karyawan : ");
+            String kategoriKaryawan = scan.nextLine();
+            System.out.println();
+            Karyawan karyawan = new Karyawan(nama, alamat, nomorTelepon, jenisKelamin, kategoriKaryawan);
+            karyawan.getInfo();
+            karyawan.tingkahLaku();
+        }
+    }
+}
---
+++ MainMobil.java
@@ -0,0 +1,28 @@
+package studikasus;
+
+import java.util.Scanner;
+
+public class MainMobil {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah data mobil: ");
+        int jumlahMobil = scan.nextInt(); scan.nextLine();
+        System.out.println();
+
+        for (int i = 0; i < jumlahMobil; i++) {
+            System.out.print("Masukkan nomor plat     : ");
+            String noPlat = scan.nextLine();
+            System.out.print("Masukkan merk mobil     : ");
+            String merkMobil = scan.nextLine();
+            System.out.print("Masukkan warna mobil    : ");
+            String warnaMobil = scan.nextLine();
+            System.out.print("Masukkan tahun produksi : ");
+            int tahunKeluaranMobil = scan.nextInt(); scan.nextLine();
+            System.out.println();
+            Mobil mobil = new Mobil(noPlat, merkMobil, warnaMobil, tahunKeluaranMobil);
+            mobil.getInfo();
+            mobil.tingkahLaku();
+            System.out.println();
+        }
+    }
+}
